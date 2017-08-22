7ae81ca4481aade777fb4f7e9036f6a909a4bea7
Ahmad Arif <ahmad.arif019@gmail.com>
2017-08-22 11:25:50 +0700

    create skeleton package

---
+++ src/ExampleServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AhmadArif\Example;
+
+use Illuminate\Support\ServiceProvider;
+
+class ExampleServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
